b25799ee0cac3a1075e3bcc64cebad86b57a43ba
Sarah Dunbar <sarahdunbar@Sarahs-MacBook-Air.local>
2017-11-29 19:32:55 -0700

    day 2 scaffold added

--- programming/day2/IntGrid2D.java
+++ programming/day2/IntGrid2D.java
@@ -14,26 +14,48 @@ upper_left_y=ul_y;
 lower_right_x=lr_x;
 lower_right_y=lr_y;
 fill=fill_;
+
+//creates length of rows and columns, add one to the difference of points to
+//get correctlength
+
 int grid_row=Math.abs(ul_x-lr_x)+1;
 int grid_column=Math.abs(ul_y-lr_y)+1;
 grid=new char[grid_row][grid_column];
+
+//fills grid with input fill value
+
 for(int i=0; i<grid_row; i++){
   for(int j=0; j<grid_column; j++){
     grid[i][j]=fill_;
   }
 }
 }
+
+//takes an IInpoint2D object and places it at the correct index in the grid
+//must subtract the upper left x and lower left y in order to get the correct
+//positive indexes for the 2D array
+
 public void setPoint(IIntPoint2D p, char v){
   grid[p.getX()-upper_left_x][p.getY()-lower_right_y]=v;
 }
+
+//gets the character value for a certain IIntPoint2D index on the grid, again
+//we must subtract the upper left x and lower right y to map this index to the
+//correct value in the 2D array
+
 public char getPoint(IIntPoint2D p){
   return grid[p.getX()-upper_left_x][p.getY()-lower_right_y];
 }
+
+//Returns the IIntPoint2D index value in the upperleft corner
 
 public IIntPoint2D getUpperLeftCorner(){
   IIntPoint2D upper_left= new IntPoint2D(upper_left_x,upper_left_y);
   return upper_left;
 }
+
+//Returns the IIntPoint2D index value in the lower right corner
+
 public IIntPoint2D getLowerRightCorner(){
   IIntPoint2D lower_right= new IntPoint2D(lower_right_x,lower_right_y);
   return lower_right;
